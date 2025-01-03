5ab9da4ff549fd2df1041e0175c4e016df1023b4
Matt Harrisburg <matthew.harrisburg@gmail.com>
2025-01-03 02:01:35 -0500

    Initialize
    Set up .env and basic framework for bot

---
+++ src/main/java/commandManager.java
@@ -0,0 +1,20 @@
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class commandManager extends ListenerAdapter {
+
+
+    @Override
+    public void onGuildReady(GuildReadyEvent event) {
+        List<CommandData> commandData = new ArrayList<>();
+        // Refreance
+        // commandData.add(Commands.slash("example", "example").addOptions(new class().getOptionsAdd().get(0)));
+
+        event.getGuild().updateCommands().addCommands(commandData).queue();
+    }
+}
+
---
+++ src/main/java/main.java
@@ -0,0 +1,43 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+
+public class main {
+    private final ShardManager shardManager;
+
+
+    Dotenv dotenv = Dotenv.load();
+
+    // Get the Discord token
+    private final String token = dotenv.get("DISCORD_TOKEN");
+    public main() throws LoginException {
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.enableIntents(GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_PRESENCES);
+        builder.setEventPassthrough(true);
+        builder.enableCache(CacheFlag.ONLINE_STATUS);
+        shardManager = builder.build();
+
+        shardManager.addEventListener(
+
+        );
+
+    }
+    public ShardManager getShardManager() { return shardManager; }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            main bot = new main();
+            System.out.println("Sucess: your bot is online");
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided bot token is invalid!");
+        }
+    }
+}
