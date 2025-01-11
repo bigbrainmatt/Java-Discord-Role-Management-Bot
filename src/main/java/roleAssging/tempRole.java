fc31ac9fff334bf41c458e9cc201a32ada90c9c3
Matt Harrisburg <matthew.harrisburg@gmail.com>
2025-01-11 03:53:29 -0500

    Update tempRole.java

--- src/main/java/roleAssging/tempRole.java
+++ src/main/java/roleAssging/tempRole.java
@@ -23,6 +23,8 @@ import static misc.miscInfo.getTime;
 
 public class tempRole extends ListenerAdapter {
 
+    private String msgID;
+
     public List<OptionData> getOptions() {
         List<OptionData> data = new ArrayList<>();
         data.add(new OptionData(OptionType.ROLE, "role", "Role needed", true));
@@ -45,61 +47,62 @@ public class tempRole extends ListenerAdapter {
             int time = event.getOption("time").getAsInt();
             String type = event.getOption("type").getAsString();
 
-            setTempTime(event,user,role,time,type);
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setColor(Color.green);
+            eb.setTitle("Temp Role Active");
+            eb.setDescription(role.getAsMention() + " has been temporarily assigned to " + user.getAsMention() + " for " + time + " " + type + " by " + event.getUser().getAsMention());
+            eb.setFooter(getTime(), event.getJDA().getSelfUser().getAvatarUrl());
 
+            event.getGuild().addRoleToMember(user,role).complete();
+
+            String chan ="";
+            try {
+                chan = getPermissionValue(event.getGuild().getId(), "0", "SERVER_LOG_CHAN");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (CsvException e) {
+                throw new RuntimeException(e);
+            }
+
+            event.getGuild().getTextChannelById(chan).sendMessageEmbeds(eb.build()).queue(message -> {
+                msgID = message.getId();
+            });
+
+            event.reply(role.getAsMention() + " has been assigned to " + user.getAsMention() + " for " + time + " " + type).setAllowedMentions(Collections.emptyList()).queue();
+
+            String dis = role.getAsMention() + " has been temporarily assigned to " + user.getAsMention() + " for " + time + " " + type + " by " + event.getUser().getAsMention();
+            String foot = getTime();
+
+            Timer timer = new Timer();
+
+            String finalChan = chan;
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                }
+            };
+
+            timer.scheduleAtFixedRate(task, 0, 1000);
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                    EmbedBuilder eb = new EmbedBuilder();
+                    eb.setColor(Color.red);
+                    eb.setTitle("Temp Role Inactive");
+                    eb.setDescription(dis);
+                    eb.setFooter(foot, event.getJDA().getSelfUser().getAvatarUrl());
+
+                    event.getGuild().getTextChannelById(finalChan).sendMessageEmbeds(eb.build()).queue();
+
+                    user.getRoles().remove(role);
+
+                    timer.cancel();
+                    System.out.println("Timer stopped.");
+                }
+            }, 10000);
         }
     }
-
-    private void setTempTime(SlashCommandInteractionEvent event, Member user, Role role, int time, String type) {
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setColor(Color.green);
-        eb.setTitle("Temp Role Active");
-        eb.setDescription(role.getAsMention() + " has been temporarily assigned to " + user.getAsMention() + " for " + time + " " + type + " by " + event.getUser().getAsMention());
-        eb.setFooter(getTime(), event.getJDA().getSelfUser().getAvatarUrl());
-
-        event.getGuild().addRoleToMember(user,role).complete();
-
-        String chan ="";
-        try {
-            chan = getPermissionValue(event.getGuild().getId(), "0", "SERVER_LOG_CHAN");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (CsvException e) {
-            throw new RuntimeException(e);
-        }
-
-        String[] messageId = {""};
-        event.getGuild().getTextChannelById(chan).sendMessageEmbeds(eb.build()).queue(message -> {
-            messageId[0] = message.getId();
-            System.out.println(messageId[0]);
-            System.out.println("Done");
-        });
-        String mID = messageId[0];
-
-        event.reply(role.getAsMention() + " has been assigned to " + user.getAsMention() + " for " + time + " " + type).setAllowedMentions(Collections.emptyList()).queue();
-
-        String dis = role.getAsMention() + " has been temporarily assigned to " + user.getAsMention() + " for " + time + " " + type + " by " + event.getUser().getAsMention();
-        String foot = getTime();
-
-        LocalDateTime twoSecondsLater = LocalDateTime.now().plusSeconds(2);
-        Date twoSecondsLaterAsDate = Date.from(twoSecondsLater.atZone(ZoneId.systemDefault()).toInstant());
-        new Timer().schedule(deactivate(role, user, event, chan, mID, dis, foot), twoSecondsLaterAsDate);
-
-
-    }
-
-    private TimerTask deactivate(Role role, Member user, SlashCommandInteractionEvent event, String chan, String mID, String dis, String foot) {
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setColor(Color.red);
-        eb.setTitle("Temp Role Inactive");
-        eb.setDescription(dis);
-        eb.setFooter(foot, event.getJDA().getSelfUser().getAvatarUrl());
-
-        event.getGuild().getTextChannelById(chan).getHistory().getMessageById(mID).editMessageEmbeds(eb.build()).queue();
-
-        user.getRoles().remove(role);
-        return null;
-    }
-
 
 }
